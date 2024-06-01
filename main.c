bec4bf05076185bc94084231fcc92bdd7aac5c9a
Raven <yakubiq@proton.me>
2024-06-01 23:19:37 +0200

    communaction fixed

--- main.c
+++ main.c
@@ -1,105 +1,63 @@
 #include <netinet/in.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
 #include <sys/socket.h>
-
+#include <sys/types.h>
 #define EXIT_SUCCESS 0
-#define EXIT_FAIL 1
-#define EXIT_FAIL_NOT_ENOUGH_ARGS 2
-#define EXIT_FAIL_PORT 3
-#define EXIT_FAIL_SOCKET_CREATE 4
-#define EXIT_FAIL_SOCKET_BIND 5
-#define EXIT_FAIL_SOCKET_LISTEN 6
-#define EXIT_FAIL_SOCKET_ACCEPT 7
-#define EXIT_FAIL_SOCKET_CONNECT 8
-#define EXIT_FAIL_SOCKET_IPCONVERSION 9
+#define EXIT_FAIL_SOCKET_CREATE 1
+#define EXIT_FAIL_SOCKET_BIND 2
+#define EXIT_FAIL_SOCKET_LISTEN 3
+#define EXIT_FAIL_SOCKET_ACCEPT 4
+#define EXIT_FAIL_SOCKET_CONNECT 5
 
 struct FLAGS {
   int port;
-  char *watchDirectory;
-  char *type;
 };
 
-int server(const struct FLAGS flags);
-int client(const struct FLAGS flags);
+int server(struct FLAGS* flags);
+int client(struct FLAGS* flags);
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv){
   struct FLAGS flags;
-  if (argc < 7) {
-    printf("Usage: %s -d [directory] -p [port] -t [type]\n", argv[0]);
-    exit(EXIT_FAIL_NOT_ENOUGH_ARGS);
-  }
-  for (int i = 1; i < 7; i += 2) {
-    const char *flag = argv[i];
-    char *value = argv[i + 1];
-    printf("flag: %s\n", flag);
-    if (!strcmp(flag, "-p")) {
-      printf("[%d] port flag reached\n", i);
-      flags.port = atoi(value);
-      if (flags.port == 0)
-        exit(EXIT_FAIL_PORT);
-    }
-    if (!strcmp(flag, "-d")) {
-      printf("[%d] dir flag reached\n", i);
-      flags.watchDirectory = value;
-    }
-    if (!strcmp(flag, "-t")) {
-      printf("[%d] type flag reached\n", i);
-      flags.type = value;
-    }
-  }
-  printf("Flag structure filled\nport:%d\nwatchDirectory:%s\ntype:%s\n",flags.port, flags.watchDirectory, flags.type);
-  if (!strcmp(flags.type, "server")) {
-    server(flags);
-  } else if (!strcmp(flags.type, "client")) {
-    client(flags);
-  }
+  flags.port = 3512;
+  client(&flags);
   exit(EXIT_SUCCESS);
 }
 
-int server(const struct FLAGS flags) {
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
+int server(struct FLAGS* flags){
+  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(serverSocket == -1)
     exit(EXIT_FAIL_SOCKET_CREATE);
-  }
-  struct sockaddr_in address;
-  socklen_t addrlen = sizeof(address);
-  bzero(&address, sizeof(address));
-  address.sin_family = AF_INET;
-  address.sin_port = htons(flags.port);
-  address.sin_addr.s_addr = htonl(INADDR_ANY);
-  if (bind(sockfd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+  struct sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons((*flags).port);
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  if(bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
     exit(EXIT_FAIL_SOCKET_BIND);
-  }
-  if (listen(sockfd, 0) == -1) {
+  if(listen(serverSocket, 1) == -1)
     exit(EXIT_FAIL_SOCKET_LISTEN);
-  }
-  struct sockaddr clientSocket;
-  if (accept(sockfd, &clientSocket, &addrlen) == -1) {
+  int clientSocket = accept(serverSocket, NULL, NULL);
+  if(clientSocket == -1)
     exit(EXIT_FAIL_SOCKET_ACCEPT);
-  }
-  puts("Connection accepted\n");
-  exit(EXIT_SUCCESS);
+  char* testmsg = "123";
+  send(clientSocket, testmsg, strlen(testmsg), 0);
+  return 0;
 }
-
-int client(const struct FLAGS flags) {
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
+int client(struct FLAGS* flags){
+  int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(clientSocket == -1)
     exit(EXIT_FAIL_SOCKET_CREATE);
-  }
-  struct sockaddr_in address;
-  socklen_t addrlen = sizeof(address);
-  bzero(&address, sizeof(address));
-  address.sin_family = AF_INET;
-  address.sin_port = htons(flags.port);
-  if(inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) == -1){
-    exit(EXIT_FAIL_SOCKET_IPCONVERSION);
-  } 
-  if(connect(sockfd, (struct sockaddr*)&address, sizeof(address)) == -1){
+  struct sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons((*flags).port);
+  serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  if(connect(clientSocket,(struct sockaddr*)&serverAddress, sizeof(serverAddress))==-1)
     exit(EXIT_FAIL_SOCKET_CONNECT);
-  }
-  exit(EXIT_SUCCESS);
+  char strdata[256];
+  recv(clientSocket, strdata, sizeof(strdata), 0);
+  printf("%s",strdata);
+
+  return 0;
 }
