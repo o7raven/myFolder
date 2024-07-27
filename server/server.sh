207f15897f5280323677804a6b73616fbcb4aad3
Raven <yakubiq@proton.me>
2024-07-28 01:23:16 +0200

    server compiling update

--- server/server.sh
+++ server/server.sh
@@ -2,10 +2,10 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc main.c
+  gcc ../src/main.c -o server.out
 fi
 
-./a.out -d null -t server -a localhost -p 3562
+./server.out -d null -t server -a localhost -p 3562
 exit=$?
 echo "Exit status: $exit"
 
