790dc070499ab386b68c678a47cf723228c3d56a
Raven <yakubiq@proton.me>
2024-07-28 01:23:01 +0200

    client script update

--- client/client.sh
+++ client/client.sh
@@ -2,10 +2,10 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc main.c 
+  gcc ../src/main.c -o client.out
 fi
 
-./a.out -d null -t client -a localhost -p 3562
+./client.out -d null -t client -a localhost -p 3562
 exit=$?
 echo "Exit status: $exit"
 
