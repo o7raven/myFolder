85ebc7ada4e640f85ab4743fa2fe8030698d01e8
Raven <yakubiq@proton.me>
2024-08-05 22:11:41 +0200

    compiler warnings added

--- client/client.sh
+++ client/client.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c -o client.out
+  gcc ../src/main.c -o client.out -Wall -Wextra
 fi
 
 ./client.out -d null -t client -a localhost -p 3562
--- server/server.sh
+++ server/server.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c -o server.out
+  gcc ../src/main.c -o server.out -Wall -Wextra
 fi
 
 ./server.out -d null -t server -a localhost -p 3562
