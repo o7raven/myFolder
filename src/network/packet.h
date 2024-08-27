cdfad7fd635b69142a62ecea0425d780d03f2241
Raven <yakubiq@proton.me>
2024-08-27 23:33:28 +0200

    moved packet structure

--- src/network/packet.h
+++ src/network/packet.h
@@ -1,7 +1,17 @@
 #ifndef PACKET_H_
 #define PACKET_H_
-#include "communication.h"
+#include "../misc/hex.h"
 
+#define MAX_FILENAME_LENGTH 126
+typedef struct{
+  char fileName[MAX_FILENAME_LENGTH];
+  uint64_t contentLength;
+}HEADER;
+
+typedef struct{
+  HEADER header;
+  char* content;
+} PACKET;
 int deletePacket(PACKET* packet);
 PACKET* makePacket(const char* fileName, const char* directory,int* errorCode);
 int printPacket(const PACKET* packet);
