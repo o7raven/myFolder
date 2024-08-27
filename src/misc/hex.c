557e9e609e4c33250c6f79bf9951941a2344cc4f
Raven <yakubiq@proton.me>
2024-08-27 23:32:56 +0200

    including error codes header

--- src/misc/hex.c
+++ src/misc/hex.c
@@ -1,1 +1,7 @@
 #include "hex.h"
+int checkHex(uint64_t n)
+{
+  uint64_t swapped = bswap_64(n);
+  printf("original: 0x%016"PRIx64"\nSwapped: 0x%016"PRIx64"\n", n, swapped);
+  return EXIT_SUCCESS;
+}
--- src/misc/hex.h
+++ src/misc/hex.h
@@ -1,6 +1,9 @@
 #ifndef HEX_H_
 #define HEX_H_
 #include <inttypes.h>
+#include <stdio.h>
 #include <byteswap.h>
+
+#include "../error/codes.h"
 int checkHex(uint64_t n);
 #endif // !HEX_H_
