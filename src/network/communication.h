9d58096beba7b5ae921e5196c3dfcaa7495c0258
Raven <yakubiq@proton.me>
2024-08-27 23:33:13 +0200

    removed packet structure

--- src/network/communication.h
+++ src/network/communication.h
@@ -5,20 +5,10 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
 #include <sys/socket.h>
 
 #include "../error/codes.h"
-#define MAX_FILENAME_LENGTH 126
-typedef struct{
-  char fileName[MAX_FILENAME_LENGTH];
-  uint64_t contentLength;
-}HEADER;
-
-typedef struct{
-  HEADER header;
-  char* content;
-} PACKET;
+#include "packet.h"
 
 int sendPacket(const int socketfd, PACKET* packet);
 PACKET* recvPacket(const int socketfd, int* errorCode);
