db86fb0a03c205f118e1506e8e58bf361a5d0413
Raven <yakubiq@proton.me>
2024-08-29 23:23:36 +0200

    server structure for easier manipulation

--- src/network/server.c
+++ src/network/server.c
@@ -1,16 +1,17 @@
 #include "server.h"
 SERVER makeServer(struct FLAGS* flags){
+  SERVER server = {0}; 
   puts("\n---server---\n");
   puts("Starting server...\n");
   // printFlags(flags);
-  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-  if(serverSocket == -1){
-    close(serverSocket);
+  server.serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if(server.serverSocket == -1){
+    close(server.serverSocket);
     fprintf(stderr, "0x%x: server socket create error\n", EXIT_FAIL_SOCKET_CREATE);
     exit(EXIT_FAIL_SOCKET_CREATE);
   }
-  if(setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
-    close(serverSocket);
+  if(setsockopt(server.serverSocket, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0){
+    close(server.serverSocket);
     fprintf(stderr, "0x%x: server socket reuse flag fail\n", EXIT_FAIL_SOCKET_REUSE);
     exit(EXIT_FAIL_SOCKET_REUSE);
   }
@@ -18,24 +19,23 @@ SERVER makeServer(struct FLAGS* flags){
   serverAddress.sin_family = AF_INET;
   serverAddress.sin_port = htons((*flags).port);
   serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-  if(bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1){
-    close(serverSocket);
+  if(bind(server.serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1){
+    close(server.serverSocket);
     fprintf(stderr, "0x%x: server socket bind fail\n", EXIT_FAIL_SOCKET_BIND);
     exit(EXIT_FAIL_SOCKET_BIND);
   }
-  if(listen(serverSocket, 1) == -1){
-    close(serverSocket);
+  if(listen(server.serverSocket, 1) == -1){
+    close(server.serverSocket);
     fprintf(stderr, "0x%x: server socket listen fail\n", EXIT_FAIL_SOCKET_LISTEN);
     exit(EXIT_FAIL_SOCKET_LISTEN);
   }
-  int clientSocket = accept(serverSocket, NULL, NULL);
-  if(clientSocket == -1){
-    close(serverSocket);
-    close(clientSocket);
+  server.clientSocket = accept(server.serverSocket, NULL, NULL);
+  if(server.clientSocket == -1){
+    close(server.serverSocket);
+    close(server.clientSocket);
     fprintf(stderr, "0x%x: server socket accept fail\n", EXIT_FAIL_SOCKET_ACCEPT);
     exit(EXIT_FAIL_SOCKET_ACCEPT);
   }
-
   // int err = EXIT_SUCCESS;
   // PACKET* packet = makePacket(fileToSend, (*flags).dir, &err);
   // if(err != EXIT_SUCCESS){
@@ -46,7 +46,8 @@ SERVER makeServer(struct FLAGS* flags){
   // if(sendPacket(clientSocket, packet) != EXIT_SUCCESS)
   //   exit(err);
   // deletePacket(packet);
-  close(serverSocket);
-  close(clientSocket);
-  exit(EXIT_SUCCESS);
+  // close(serverSocket);
+  // close(clientSocket);
+  // exit(EXIT_SUCCESS);
+  return server;
 }
--- src/network/server.h
+++ src/network/server.h
@@ -13,7 +13,10 @@
 #include "../error/codes.h"
 
 typedef struct{
-  
+  int serverSocket;
+  int clientSocket;
+  PACKET* packet;
 } SERVER;
 SERVER makeServer(struct FLAGS* flags);
+int deleteSever(SERVER server);
 #endif // !SERVER_H_
