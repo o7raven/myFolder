fa1019e961d6d1d1f8d2d543284db3e444cbe36e
Raven <yakubiq@proton.me>
2024-08-29 23:05:55 +0200

    gcc update

--- client/client.sh
+++ client/client.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c -o client.out -Wall -Wextra
+  gcc ../src/main.c ../src/misc/*.c ../src/network/*.c -o client.out -Wall -Wextra
 fi
 
 ./client.out -d testingdirectory -t client -a localhost -p 3562
--- server/server.sh
+++ server/server.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c -o server.out -Wall -Wextra
+  gcc ../src/main.c ../src/network/*.c ../src/misc/*.c -o server.out -Wall -Wextra
 fi
 
 ./server.out -d testingdirectory -t server -a localhost -p 3562
