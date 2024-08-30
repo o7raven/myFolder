95a825068507a182d893123af923be9ef3135741
Raven <yakubiq@proton.me>
2024-08-31 00:41:32 +0200

    struct renaming

--- src/network/packet.c
+++ src/network/packet.c
@@ -1,104 +1,53 @@
 #include "packet.h"
 #include <string.h>
-int makeServerPacket(SERVER* server,const char* fileName){
+int makePacket(AGENT* agent,const char* fileName){
   puts("\n---makePacket---\n");
-  const int fileLocationLength = strlen(fileName)+strlen(server->directory)+2; 
+  const int fileLocationLength = strlen(fileName)+strlen(agent->directory)+2; 
   char* fileLocation = malloc(fileLocationLength);
   if(fileLocation == NULL){
     fprintf(stderr,"0x%x: makePacket filelocation malloc error\n", EXIT_FAIL_MALLOC);
     free(fileLocation);
     exit(EXIT_FAIL_MALLOC);
   }
-  snprintf(fileLocation, fileLocationLength,"%s/%s", server->directory, fileName); 
+  snprintf(fileLocation, fileLocationLength,"%s/%s", agent->directory, fileName); 
   printf(" LOCATION = %s\n", fileLocation);
   FILE* file = fopen(fileLocation, "rb");
   free(fileLocation);
-  puts("passed\n");
+puts("passed\n");
   if(file == NULL){
     exit(EXIT_FAIL_FILE_OPEN);
   }
   puts("passed\n");
-  server->packet = malloc(sizeof(PACKET));
-  if(server->packet==NULL){
+  agent->packet = malloc(sizeof(PACKET));
+  if(agent->packet==NULL){
     fprintf(stderr,"0x%x: makePacket packet malloc error\n", EXIT_FAIL_MALLOC);
     exit(EXIT_FAIL_MALLOC);
   }
-  strncpy(server->packet->header.fileName, fileName, MAX_FILENAME_LENGTH);
+  strncpy(agent->packet->header.fileName, fileName, MAX_FILENAME_LENGTH);
   // packet->header.fileName[MAX_FILENAME_LENGTH-1] = '\0';
   fseek(file, 0L, SEEK_END);
-  server->packet->header.contentLength = ftell(file);
+  agent->packet->header.contentLength = ftell(file);
   fseek(file, 0L, SEEK_SET);
-  server->packet->content = malloc(server->packet->header.contentLength);
+  agent->packet->content = malloc(agent->packet->header.contentLength);
   // packet->content = malloc(packet->header.contentLength+1);
-  if(server->packet->content==NULL){
+  if(agent->packet->content==NULL){
     fprintf(stderr,"0x%x: makePacket packet->content malloc error\n", EXIT_FAIL_MALLOC);
-    free(server->packet);
+    free(agent->packet);
     exit(EXIT_FAIL_MALLOC);
   }
   puts("reading content...\n");
-  size_t bytesRead = fread(server->packet->content, sizeof(char), server->packet->header.contentLength, file);
-  if(bytesRead!= server->packet->header.contentLength){
+  size_t bytesRead = fread(agent->packet->content, sizeof(char), agent->packet->header.contentLength, file);
+  if(bytesRead!= agent->packet->header.contentLength){
     fprintf(stderr, "0x%x: makePacket file read error\n", EXIT_FAIL_FILE_READ);
     fclose(file);
-    deletePacket(server->packet);
+    deletePacket(agent->packet);
     exit(EXIT_FAIL_FILE_READ);
   }
   // packet->content[packet->header.contentLength] = '\0';
   fclose(file);
-  printPacket(server->packet);
-  checkHex(server->packet->header.contentLength);
-  server->packet->header.contentLength = bswap_64(server->packet->header.contentLength);
-  return EXIT_SUCCESS;
-}
-
-int makeClientPacket(CLIENT* client,const char* fileName){
-  puts("\n---makePacket---\n");
-  const int fileLocationLength = strlen(fileName)+strlen(client->directory)+2; 
-  char* fileLocation = malloc(fileLocationLength);
-  if(fileLocation == NULL){
-    fprintf(stderr,"0x%x: makePacket filelocation malloc error\n", EXIT_FAIL_MALLOC);
-    free(fileLocation);
-    exit(EXIT_FAIL_MALLOC);
-  }
-  snprintf(fileLocation, fileLocationLength,"%s/%s", client->directory, fileName); 
-  printf(" LOCATION = %s\n", fileLocation);
-  FILE* file = fopen(fileLocation, "rb");
-  free(fileLocation);
-  puts("passed\n");
-  if(file == NULL){
-    exit(EXIT_FAIL_FILE_OPEN);
-  }
-  puts("passed\n");
-  client->packet = malloc(sizeof(PACKET));
-  if(client->packet==NULL){
-    fprintf(stderr,"0x%x: makePacket packet malloc error\n", EXIT_FAIL_MALLOC);
-    exit(EXIT_FAIL_MALLOC);
-  }
-  strncpy(client->packet->header.fileName, fileName, MAX_FILENAME_LENGTH);
-  // packet->header.fileName[MAX_FILENAME_LENGTH-1] = '\0';
-  fseek(file, 0L, SEEK_END);
-  client->packet->header.contentLength = ftell(file);
-  fseek(file, 0L, SEEK_SET);
-  client->packet->content = malloc(client->packet->header.contentLength);
-  // packet->content = malloc(packet->header.contentLength+1);
-  if(client->packet->content==NULL){
-    fprintf(stderr,"0x%x: makePacket packet->content malloc error\n", EXIT_FAIL_MALLOC);
-    free(client->packet);
-    exit(EXIT_FAIL_MALLOC);
-  }
-  puts("reading content...\n");
-  size_t bytesRead = fread(client->packet->content, sizeof(char), client->packet->header.contentLength, file);
-  if(bytesRead!= client->packet->header.contentLength){
-    fprintf(stderr, "0x%x: makePacket file read error\n", EXIT_FAIL_FILE_READ);
-    fclose(file);
-    deletePacket(client->packet);
-    exit(EXIT_FAIL_FILE_READ);
-  }
-  // packet->content[packet->header.contentLength] = '\0';
-  fclose(file);
-  printPacket(client->packet);
-  checkHex(client->packet->header.contentLength);
-  client->packet->header.contentLength = bswap_64(client->packet->header.contentLength);
+  printPacket(agent->packet);
+  checkHex(agent->packet->header.contentLength);
+  agent->packet->header.contentLength = bswap_64(agent->packet->header.contentLength);
   return EXIT_SUCCESS;
 }
 int deletePacket(PACKET* packet){
