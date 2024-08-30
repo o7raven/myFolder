69a653813e663762ffde29f10f233ceaa1c0d390
Raven <yakubiq@proton.me>
2024-08-31 00:45:13 +0200

    changed gcc args

--- client/client.sh
+++ client/client.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c ../src/misc/*.c ../src/network/*.c -o client.out -Wall -Wextra
+  gcc ../src/*.c ../src/structures.h ../src/misc/*.c ../src/network/*.c -o client.out -Wall -Wextra
 fi
 
 ./client.out -d testingdirectory -t client -a localhost -p 3562
--- server/server.sh
+++ server/server.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "comp" ]]; then
   echo "compiling..."
-  gcc ../src/main.c ../src/network/*.c ../src/misc/*.c -o server.out -Wall -Wextra
+  gcc ../src/*.c ../src/structures.h ../src/network/*.c ../src/misc/*.c -o server.out -Wall -Wextra
 fi
 
 ./server.out -d testingdirectory -t server -a localhost -p 3562
