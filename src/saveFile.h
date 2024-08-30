3ba9b2a4db9effa51dee54ba3ba2ad5a3c673fe0
Raven <yakubiq@proton.me>
2024-08-30 23:00:07 +0200

    added includes

--- src/saveFile.h
+++ src/saveFile.h
@@ -1,6 +1,9 @@
 #ifndef saveFile_H_
 #define saveFile_H_
+#include <unistd.h>
 #include "network/packet.h"
-int saveFile(const PACKET* packet);
+#include "misc/notifications.h"
+#include "structures.h"
+int saveFile(CLIENT* client, const char* directory);
 
 #endif // !saveFile_H_
