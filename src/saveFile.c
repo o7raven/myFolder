7dc555ccbbe64168593ec4d805d54944b98581ba
Raven <yakubiq@proton.me>
2024-08-30 23:00:22 +0200

    ported to use the client structure

--- src/saveFile.c
+++ src/saveFile.c
@@ -1,36 +1,38 @@
 #include "saveFile.h"
 
-int saveFIle(const PACKET*){
+int saveFile(CLIENT* client, const char* directory){
   puts("fopen section...\n");
-  const int fileLocationLength = strlen(fileToSend)+strlen((*flags).dir)+2; 
+  const int fileLocationLength = strlen(client->packet->header.fileName)+strlen(directory)+2; 
   char* fileLocation = malloc(fileLocationLength);
   if(fileLocation == NULL){
     fprintf(stderr,"0x%x: makePacket filelocation malloc error\n", EXIT_FAIL_MALLOC);
     free(fileLocation);
     return EXIT_FAIL_MALLOC;
   }
-  snprintf(fileLocation, fileLocationLength,"%s/%s", (*flags).dir, fileToSend); 
+  snprintf(fileLocation, fileLocationLength,"%s/%s", directory, client->packet->header.fileName); 
   // FILE* newFile = fopen(receivedPacket->header.fileName, "wb");
   FILE* newFile = fopen(fileLocation, "wb");
   if(newFile == NULL){
-    close(clientSocket);
-    deletePacket(receivedPacket);
-    fprintf(stderr, "0x%x: client fail file open\nfilename:%s\n", EXIT_FAIL_FILE_OPEN, receivedPacket->header.fileName);
+    close(client->clientSocket);
+    deletePacket(client->packet);
+    fprintf(stderr, "0x%x: client fail file open\nfilename:%s\n", EXIT_FAIL_FILE_OPEN, client->packet->header.fileName);
     return EXIT_FAIL_FILE_OPEN;
   }
   puts("passed\n");
   puts("fwrite section...\n");
-  uint64_t bytesWritten = fwrite(receivedPacket->content, sizeof(char), receivedPacket->header.contentLength, newFile);
-  if(bytesWritten != receivedPacket->header.contentLength){
-    close(clientSocket);
-    deletePacket(receivedPacket);
-    fprintf(stderr, "0x%x: client file written characters:%"PRIu64"d\ncharacters to be written:%zu\n", EXIT_FAIL_FWRITE, bytesWritten, receivedPacket->header.contentLength);
+  uint64_t bytesWritten = fwrite(client->packet->content, sizeof(char), client->packet->header.contentLength, newFile);
+  if(bytesWritten != client->packet->header.contentLength){
+    close(client->clientSocket);
+    deletePacket(client->packet);
+    fprintf(stderr, "0x%x: client file written characters:%"PRIu64"d\ncharacters to be written:%zu\n", EXIT_FAIL_FWRITE, bytesWritten, client->packet->header.contentLength);
     return EXIT_FAIL_FWRITE;
   }
   puts("passed\n");
   fclose(newFile);
   puts("mallocing message section...\n");
   puts("passed\n");
-  notify(receivedPacket->header.fileName);
+  notify(client->packet->header.fileName);
+  return EXIT_SUCCESS;
 }
+
 
