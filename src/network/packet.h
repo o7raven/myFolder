c052b761b63bb48855ec44aab908d4f1ea865112
Raven <yakubiq@proton.me>
2024-08-31 00:05:53 +0200

    function overloading added

--- src/network/packet.h
+++ src/network/packet.h
@@ -2,6 +2,8 @@
 #define PACKET_H_
 #include "../misc/hex.h"
 #include "../structures.h"
+
+#define makePacket(type, fileName) _Generic(type, SERVER*: makeServerPacket, CLIENT*: makeClientPacket)(type, fileName)
 int deletePacket(PACKET* packet);
 int makeServerPacket(SERVER* server,const char* fileName);
 int makeClientPacket(CLIENT* client, const char* fileName);
