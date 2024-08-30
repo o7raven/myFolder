826aa5de90a0590fa270249202284eb03d115d27
Raven <yakubiq@proton.me>
2024-08-31 00:26:49 +0200

    renaming to agent

--- src/network/packet.h
+++ src/network/packet.h
@@ -3,10 +3,9 @@
 #include "../misc/hex.h"
 #include "../structures.h"
 
-#define makePacket(type, fileName) _Generic(type, SERVER*: makeServerPacket, CLIENT*: makeClientPacket)(type, fileName)
+// #define makePacket(type, fileName) _Generic(type, SERVER*: makeServerPacket, CLIENT*: makeClientPacket)(type, fileName)
 int deletePacket(PACKET* packet);
-int makeServerPacket(SERVER* server,const char* fileName);
-int makeClientPacket(CLIENT* client, const char* fileName);
+int makePacket(AGENT* agent, const char* fileName);
 int printPacket(const PACKET* packet);
 
 #endif // !PACKET_H_
